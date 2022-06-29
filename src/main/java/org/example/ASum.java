952fc4e334e2c84f7ca124bf85fceab2d6285873
Bart <Bartimeuse95@ya.ru>
2022-06-29 16:30:16 +0700

    Origin

--- src/main/java/org/example/ASum.java
+++ src/main/java/org/example/ASum.java
@@ -2,6 +2,8 @@ package org.example;
 
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.TreeMap;
 
 public class ASum {
     public static long findNb(long m) {
@@ -57,5 +59,43 @@ public class ASum {
           dArr.append(" ");
         }
         return dArr.toString().replaceAll(".$", "");
+    }
+
+    private static TreeMap<Integer, String> MAP;
+    static {
+        MAP = new TreeMap<Integer, String>(Collections.reverseOrder());
+        MAP.put( 1, "I" );
+        MAP.put( 4, "IV" );
+        MAP.put( 5, "V" );
+        MAP.put( 9, "IX" );
+        MAP.put( 10, "X" );
+        MAP.put( 40, "XL" );
+        MAP.put( 50, "L" );
+        MAP.put( 90, "XC" );
+        MAP.put( 100, "C" );
+        MAP.put( 400, "CD" );
+        MAP.put( 500, "D" );
+        MAP.put( 900, "CM" );
+        MAP.put( 1000, "M" );
+    }
+
+    public String solution(int n) {
+        return "I".repeat(n)
+                .replace("IIIII", "V").replace("IIII", "IV")
+                .replace("VV", "X").replace("VIV", "IX")
+                .replace("XXXXX", "L").replace("XXXX", "XL")
+                .replace("LL", "C").replace("LXL", "XC")
+                .replace("CCCCC", "D").replace("CCCC", "CD")
+                .replace("DD", "M").replace("DCD", "CM");
+        //        StringBuilder builder = new StringBuilder();
+//        for(Map.Entry<Integer, String> entry: MAP.entrySet() ){
+//            int i = entry.getKey();
+//            String s = entry.getValue();
+//            while( n>=i ){
+//                builder.append(s);
+//                n -= i;
+//            }
+//        }
+//        return builder.toString();
     }
 }
